ff865fd4f5a069d577f55f527d6bd524462b12e0
Sofia Rojas <sofia.rojasflo@gmail.com>
2021-04-15 20:58:25 -0500

    deploying html, js and css files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Work-Day-Scheduler
+# Work-day-scheduler
+
+## Description
+The project was made in order to build a work day scheduler, making it easier for a busy employer to plan for its day to day activities. The advantage of the following scheduler is that allows the user to save the notes in their local storage and keep them display.
+
+## Project status
+This project is in its testing phase. 
+
+## Installation
+ * Create a repo on GitHub
+ * Clone the repo on the computer
+ * Create an HTML, CSS and Javascript file
+ * Write the HTML and CSS code previously provided
+ * Write the jQuery code
+ * Use moment in order to select the time and date and display it
+ * Use google DevTools in order to inspect if the code is running correctly
+ * Pull and commit changes on Github
+ * Publish the website on Github pages
+
+## Finished Project
+
+![WorkDaySchedulerScreenshot.](./Scheduler-SS.PNG)
+
+## Link to the website
+https://sofiacrf.github.io/Work-Day-Scheduler/
---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  // Use Moment.js to display today´s date
+  var today = moment();
+  $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+  // on click event to save in local storage
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings("description").val();
+    var hour = $(this).parent().attr("id");
+
+    localStorage.setItem(hour, text);
+  });
+
+  // function that displays the class based on your current time
+  function hourSelector() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeElement = parseInt($(this).attr("id").split("-")[1]);
+
+      if (timeElement < currentTime) {
+        $(this).addClass("past");
+      } else if (timeElement === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  hourSelector();
+
+  //load the text description from local storage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+});
+
+console.log($("#hour-17 .description").val(localStorage.getItem("hour-17")));
